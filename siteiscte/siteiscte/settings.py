0f2507473e91aa8968a7a52d736707f629b8d39b
avsmm-iscte <avsmm@iscte-iul.pt>
2023-03-31 14:38:06 +0100

    update

--- siteiscte/siteiscte/settings.py
+++ siteiscte/siteiscte/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 import os
-MEDIA_URL = '/nomedaapp/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'nomedaapp/static/media')
+MEDIA_URL = '/votacao/static/votacao/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'votacao/static/votacao/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
