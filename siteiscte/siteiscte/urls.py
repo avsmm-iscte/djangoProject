a66aceba930cfdf395f5cc12dbef1c369846b34f
JoGui2014 <gvloz@iscte-iul.pt>
2023-04-01 21:51:37 +0100

    update Gui

--- siteiscte/siteiscte/urls.py
+++ siteiscte/siteiscte/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+ path('', include('votacao.urls')),
  path('votacao/', include('votacao.urls')),
  path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL,
-  document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
