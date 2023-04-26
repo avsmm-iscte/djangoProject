36bfd70715b03a40577715f2076c41903b35f0e0
avsmm-iscte <avsmm@iscte-iul.pt>
2023-04-26 12:16:09 +0100

    semana9

--- siteiscte/votacao/urls.py
+++ siteiscte/votacao/urls.py
@@ -31,4 +31,9 @@ urlpatterns = [
 
     path('fazer_upload', views.fazer_upload, name='fazer_upload'),
 
+    path('api/questoes/', views.questoes_lista),
+    path('api/questoes/<int:pk>', views.questoes_edita),
+    path('api/opcoes/', views.opcoes_lista),
+    path('api/opcoes/<int:pk>', views.opcoes_edita),
+
 ]
